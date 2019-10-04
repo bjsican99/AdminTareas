99761fed25d36f139b0c0c7564f5a19227497abb
unknown <francisco2015franco@gmail.com>
2019-10-03 22:33:06 -0600

    Correccion Codigo

--- AdministradorProce/Form1.Designer.cs
+++ AdministradorProce/Form1.Designer.cs
@@ -28,13 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.listBox2 = new System.Windows.Forms.ListBox();
             this.listBox3 = new System.Windows.Forms.ListBox();
             this.listBox4 = new System.Windows.Forms.ListBox();
             this.listBox5 = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnKill = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -42,51 +43,57 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(47, 57);
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(70, 89);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(195, 340);
+            this.listBox1.Size = new System.Drawing.Size(290, 529);
             this.listBox1.TabIndex = 0;
             // 
             // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 16;
-            this.listBox2.Location = new System.Drawing.Point(270, 57);
+            this.listBox2.ItemHeight = 25;
+            this.listBox2.Location = new System.Drawing.Point(405, 89);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(195, 340);
+            this.listBox2.Size = new System.Drawing.Size(290, 529);
             this.listBox2.TabIndex = 1;
             // 
             // listBox3
             // 
             this.listBox3.FormattingEnabled = true;
-            this.listBox3.ItemHeight = 16;
-            this.listBox3.Location = new System.Drawing.Point(498, 57);
+            this.listBox3.ItemHeight = 25;
+            this.listBox3.Location = new System.Drawing.Point(747, 89);
+            this.listBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.listBox3.Name = "listBox3";
-            this.listBox3.Size = new System.Drawing.Size(195, 340);
+            this.listBox3.Size = new System.Drawing.Size(290, 529);
             this.listBox3.TabIndex = 2;
             // 
             // listBox4
             // 
             this.listBox4.FormattingEnabled = true;
-            this.listBox4.ItemHeight = 16;
-            this.listBox4.Location = new System.Drawing.Point(725, 57);
+            this.listBox4.ItemHeight = 25;
+            this.listBox4.Location = new System.Drawing.Point(1088, 89);
+            this.listBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.listBox4.Name = "listBox4";
-            this.listBox4.Size = new System.Drawing.Size(195, 340);
+            this.listBox4.Size = new System.Drawing.Size(290, 529);
             this.listBox4.TabIndex = 3;
             // 
             // listBox5
             // 
             this.listBox5.FormattingEnabled = true;
-            this.listBox5.ItemHeight = 16;
-            this.listBox5.Location = new System.Drawing.Point(951, 57);
+            this.listBox5.ItemHeight = 25;
+            this.listBox5.Location = new System.Drawing.Point(1426, 89);
+            this.listBox5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.listBox5.Name = "listBox5";
-            this.listBox5.Size = new System.Drawing.Size(195, 340);
+            this.listBox5.Size = new System.Drawing.Size(290, 529);
             this.listBox5.TabIndex = 4;
             // 
             // button1
@@ -94,34 +101,36 @@
             this.button1.AutoSize = true;
             this.button1.BackColor = System.Drawing.Color.Black;
             this.button1.Image = global::AdministradorProce.Properties.Resources.icons8_actualizar_64;
-            this.button1.Location = new System.Drawing.Point(68, 441);
-            this.button1.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.button1.Location = new System.Drawing.Point(102, 689);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(116, 80);
+            this.button1.Size = new System.Drawing.Size(174, 125);
             this.button1.TabIndex = 5;
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // btnKill
             // 
-            this.button2.AutoSize = true;
-            this.button2.BackColor = System.Drawing.Color.Black;
-            this.button2.Image = global::AdministradorProce.Properties.Resources.icons8_eliminar_64;
-            this.button2.Location = new System.Drawing.Point(240, 440);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(121, 81);
-            this.button2.TabIndex = 6;
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.btnKill.AutoSize = true;
+            this.btnKill.BackColor = System.Drawing.Color.Black;
+            this.btnKill.Image = global::AdministradorProce.Properties.Resources.icons8_eliminar_64;
+            this.btnKill.Location = new System.Drawing.Point(360, 688);
+            this.btnKill.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnKill.Name = "btnKill";
+            this.btnKill.Size = new System.Drawing.Size(182, 127);
+            this.btnKill.TabIndex = 6;
+            this.btnKill.UseVisualStyleBackColor = false;
+            this.btnKill.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
             this.button3.AutoSize = true;
             this.button3.BackColor = System.Drawing.Color.Black;
             this.button3.Image = global::AdministradorProce.Properties.Resources.icons8_salida_64;
-            this.button3.Location = new System.Drawing.Point(1024, 430);
+            this.button3.Location = new System.Drawing.Point(1536, 672);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(122, 91);
+            this.button3.Size = new System.Drawing.Size(183, 142);
             this.button3.TabIndex = 7;
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
@@ -132,9 +141,10 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(44, 19);
+            this.label1.Location = new System.Drawing.Point(66, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(183, 32);
+            this.label1.Size = new System.Drawing.Size(279, 52);
             this.label1.TabIndex = 8;
             this.label1.Text = "PROCESOS";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -145,9 +155,10 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(252, 19);
+            this.label2.Location = new System.Drawing.Point(378, 30);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(243, 32);
+            this.label2.Size = new System.Drawing.Size(378, 52);
             this.label2.TabIndex = 9;
             this.label2.Text = "IDENTIFICADOR";
             // 
@@ -157,9 +168,10 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label3.Location = new System.Drawing.Point(501, 19);
+            this.label3.Location = new System.Drawing.Point(752, 30);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(80, 32);
+            this.label3.Size = new System.Drawing.Size(124, 52);
             this.label3.TabIndex = 10;
             this.label3.Text = "RAM";
             // 
@@ -169,9 +181,10 @@
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label4.Location = new System.Drawing.Point(719, 19);
+            this.label4.Location = new System.Drawing.Point(1078, 30);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(181, 32);
+            this.label4.Size = new System.Drawing.Size(279, 52);
             this.label4.TabIndex = 11;
             this.label4.Text = "M. VIRTUAL";
             // 
@@ -181,9 +194,10 @@
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label5.Location = new System.Drawing.Point(945, 19);
+            this.label5.Location = new System.Drawing.Point(1418, 30);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(225, 32);
+            this.label5.Size = new System.Drawing.Size(345, 52);
             this.label5.TabIndex = 12;
             this.label5.Text = "PROCESADOR";
             // 
@@ -193,19 +207,24 @@
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label6.Location = new System.Drawing.Point(463, 454);
+            this.label6.Location = new System.Drawing.Point(694, 709);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(32, 32);
+            this.label6.Size = new System.Drawing.Size(48, 52);
             this.label6.TabIndex = 13;
             this.label6.Text = "0";
             // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImage = global::AdministradorProce.Properties.Resources._403441;
-            this.ClientSize = new System.Drawing.Size(1211, 577);
+            this.ClientSize = new System.Drawing.Size(1846, 965);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -213,13 +232,14 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnKill);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox5);
             this.Controls.Add(this.listBox4);
             this.Controls.Add(this.listBox3);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -236,7 +256,7 @@
         private System.Windows.Forms.ListBox listBox4;
         private System.Windows.Forms.ListBox listBox5;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnKill;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
@@ -244,6 +264,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- AdministradorProce/Form1.cs
+++ AdministradorProce/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,47 +8,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Diagnostics;
 
-namespace AdministradorProce
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
+namespace AdministradorProce{
+
+    public partial class Form1 : Form{
+
+        public Form1(){
             InitializeComponent();
             ActualizarProcesos();
-
+            timer1.Enabled = true;
         }
 
-        private void ActualizarProcesos()
-        {
-
+        private void ActualizarProcesos(){
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            listBox4.Items.Clear();
+            listBox5.Items.Clear();
+            int contador = 1;
+            foreach (Process pro in Process.GetProcesses())
+            {
+            listBox1.Items.Add(contador + ":" + pro.ProcessName); // nombre del proceso
+            listBox2.Items.Add(contador + ": " + pro.Id);// id del proceso
+            listBox3.Items.Add(contador + ": " + pro.WorkingSet64);// RAM del proceso
+            listBox4.Items.Add(contador + ": " + pro.VirtualMemorySize64); // MEmoria virtual del proceso
+            listBox5.Items.Add(contador + ": " + pro.SessionId); // CPU que usa el proceso
+                contador += 1;
+            }
+            label6.Text = "Total de Procesos:  " + listBox1.Items.Count.ToString();
         }
-        private void label1_Click(object sender, EventArgs e)
-        {
-
+        private void label1_Click(object sender, EventArgs e){
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
+        private void button1_Click(object sender, EventArgs e){
             ActualizarProcesos();
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            
-        }
+        private void button2_Click(object sender, EventArgs e){
+            try{
+                foreach (Process pro in Process.GetProcesses()){
+                    string strSelec = listBox1.SelectedItem.ToString(); // proceso seleccionado en el list box
+                    string[] strProceso = strSelec.Split(':');// divido el contenido del listbox
 
-        private void button3_Click(object sender, EventArgs e)
-        {
+                    if (pro.ProcessName == strProceso[1]){
+                        pro.Kill(); // elimina el proceso
+                    }
+                }
+                ActualizarProcesos();
+            }
+            catch (Exception ex){
+                MessageBox.Show("Ningun Proceso Seleccionado " + ex,"Error Al Eliminar el Proceso",MessageBoxButtons.OK);
+                ActualizarProcesos();
+            }
+        }
+        
+        private void button3_Click(object sender, EventArgs e){
             Close();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+        private void Form1_Load(object sender, EventArgs e){
+        }
 
+        private void timer1_Tick(object sender, EventArgs e){
+            //ActualizarProcesos();
         }
     }
 }
